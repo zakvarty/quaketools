6ae19d43b4feb238875861386925b3e80d5a6437
Zak D Varty <44709288+zakvarty@users.noreply.github.io>
2023-02-15 17:17:31 +0000

    test that rgpd is reproducible when seed is set.

---
+++ tests/testthat/test-rgpd.R
@@ -0,0 +1,7 @@
+test_that("setting the seed works", {
+  set.seed(1234)
+  gpd_sample_2 <- rgpd(n = 5, scale = 1, shape = 0, shift = 0)
+  set.seed(1234)
+  gpd_sample_1 <- rgpd(n = 5, scale = 1, shape = 0, shift = 0)
+  expect_equal(gpd_sample_1, gpd_sample_2)
+})
